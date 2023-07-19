0fcc56121d83af6fc46f394b7a898a5c9713ba3a
amrutha5699 <amruthavalli5699@gmail.com>
2023-07-19 14:50:04 +0530

    second commit

---
+++ src/demo/examplegit/git.java
@@ -0,0 +1,8 @@
+package demo.examplegit;
+
+public class git {
+	public static void main (String []args) {
+		System.out.println("hello ammi");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author pedasingu
+ *
+ */
+module demo {
+}
